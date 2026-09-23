10f8f05e4a2e982f2b39cc16027d617806e8dbda
Adam <husni.abualrub@gmail.com>
2026-09-23 18:46:20 +0400

    when we click on reset button all items will be removed

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@ import { ref } from 'vue'
 // states and functions
 const text_value = ref("")
 const submitted_text = ref("")
-const items = ref([{ message: 'Foo' }, { message: 'Bar' },{ message: submitted_text }])
+const items = ref([{ message: 'Foo' }, { message: 'Bar' },])
 
-// function Add() {
-//   submitted_text.value = text_value.value
-// }
 function Add() {
  items.value.push({ message: text_value.value}) 
+}
+function reset() {
+items.value = []
 }
 
 </script>
@@ -19,6 +19,7 @@ function Add() {
   
   <input v-model="text_value" type="text" placeholder="type here">
   <button @click="Add">submit</button>
+  <button @click="reset">reset</button>
   <h2>{{ submitted_text}}</h2>
   <li v-for="item in items">
   {{ item.message }}
