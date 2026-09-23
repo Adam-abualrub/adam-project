500da1061f32c58f868146388634b69368888eed
Adam <husni.abualrub@gmail.com>
2026-09-23 19:29:26 +0400

    thr item will be added based on the button that is clicked on it

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,20 @@ import { ref } from 'vue'
 // states and functions
 const text_value = ref("")
 const submitted_text = ref("")
-const items = ref([{ message: 'Foo' }, { message: 'Bar' },])
+const school_items = ref([])
+const chore_items = ref([])
 
-function Add() {
- items.value.push({ message: text_value.value}) 
+function Add_school() {
+ school_items.value.push({ message: text_value.value}) 
 }
-function reset() {
-items.value = []
+ function Add_chore() {
+ chore_items.value.push({ chore_message: text_value.value}) 
+}
+function school_reset() {
+school_items.value = []
+}
+function chore_reset() {
+chore_items.value = []
 }
 
 </script>
@@ -18,10 +25,19 @@ items.value = []
 <template>
   
   <input v-model="text_value" type="text" placeholder="type here">
-  <button @click="Add">submit</button>
+  <button @click="Add_school">add school</button>
+  <button @click="Add_chore">add chore</button>
+  
   <button @click="reset">reset</button>
-  <h2>{{ submitted_text}}</h2>
-  <li v-for="item in items">
+  <h2>school</h2> 
+  <li v-for="item in school_items">
   {{ item.message }}
 </li>
+
+<h2>chore</h2>
+  
+  <li v-for="item_chore in chore_items">
+   {{ item_chore.chore_message }} 
+</li>
+
 </template>
